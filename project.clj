54b9e5655ccb7094d6dbc936e9e18f1c97d0ac57
J Irving <j@lollyshouse.net>
2012-08-17 21:39:06 -0400

    Some OAA query stuff.

--- project.clj
+++ project.clj
@@ -1,7 +1,13 @@
 (defproject clj-playground "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :main clj-playground.core)
+  :description "A place for fiddling"
+  :url "http://j0ni.ca"
+  :license {:name "GPL v3"
+            :url "http://opensource.org/licenses/gpl-3.0.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.novemberain/monger "1.1.2"]
+                 [clj-time "0.4.3"]
+                 [compojure "1.1.1"]
+                 [clj-http "0.5.3"]]
+  :profiles {:dev {:plugins [[lein-ring "0.7.3"]]}}
+  :main clj-playground.core
+  :ring {:handler clj-playground.core/ring-handler})
--- src/clj_playground/core.clj
+++ src/clj_playground/core.clj
@@ -1,7 +1,30 @@
 (ns clj-playground.core
-  (:gen-class))
+  (:gen-class)
+  (:require (clj-time
+             [core :as time]
+             [format :as format])
+            (monger
+             [core :as mg]
+             [collection :as mc]
+             [query :as mq])
+            (clj-http
+             [client :as http])))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "I boot the world!"
   [& args]
-  (println "Hello, World!"))
+  (println "One day, I will run a bunch of examples!"))
+
+(def query-url
+  "This is the format string for pulling the NOAA query"
+  "http://www.weather.gov/forecasts/xml/sample_products/browser_interface/ndfdBrowserClientByDay.php?lat=%d&lon=%d&format=24+hourly&numDays=%d&startDate=%s")
+
+;; 4 days outlook hardcoded FIXME
+(defn format-query [lat lng start]
+  (format query-url lat lng 4 start))
+
+(defn now-str []
+  (format/unparse (format/formatters :date-time) (time/now)))
+
+(defn get-point-forecast [lat lng]
+  (http/get (format-query lat lng (now-str))))
